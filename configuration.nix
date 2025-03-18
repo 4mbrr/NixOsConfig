40f1e8ec66548354b44620a23d9c1981344bdfd5
Amber <uhjk5476@gmail.com>
2025-03-18 14:15:46 -0500

    Vim lsp

--- configuration.nix
+++ configuration.nix
@@ -1,164 +1,119 @@
-# Edit this configuration file to define what should be installed on
-# your system.  Help is available in the configuration.nix(5) man page
-# and in the NixOS manual (accessible by running ‘nixos-help’).
-
 { config, pkgs, ... }:
 
 {
-  imports =
-    [ # Include the results of the hardware scan.
-      ./hardware-configuration.nix
-    ];
+    imports =
+        [ 
+            ./hardware-configuration.nix
+        ];
 
   # Bootloader.
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
+    boot.loader.systemd-boot.enable = true;
+    boot.loader.efi.canTouchEfiVariables = true;
 
-  networking.hostName = "nixos"; # Define your hostname.
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+    networking.hostName = "nixos"; 
+    time.timeZone = "America/Chicago";
+    i18n.defaultLocale = "en_US.UTF-8";
 
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+    #services.xserver.enable = true;
+    #services.xserver.libinput.enable = true;
+    #jack.enable = true;
+    #media-session.enable = true;
+    hardware.bluetooth.enable = true; # enables support for Bluetooth
+    hardware.bluetooth.powerOnBoot = true; # powers up the default Bluetooth controller on boot
+    services.blueman.enable = true;
 
-  # Enable networking
-  networking.networkmanager.enable = true;
+    services = {
+        xserver.enable = true;
+        xserver.displayManager.gdm.enable = true;
+        xserver.desktopManager.gnome.enable = true;
+        syncthing.enable = true;
+    };
 
-  # Set your time zone.
-  time.timeZone = "America/Chicago";
-
-  # Select internationalisation properties.
-  i18n.defaultLocale = "en_US.UTF-8";
-
-  # Enable the X11 windowing system.
-  # You can disable this if you're only using the Wayland session.
-  #services.xserver.enable = true;
-
-  # Enable bluetooth
-  hardware.bluetooth.enable = true; # enables support for Bluetooth
-  hardware.bluetooth.powerOnBoot = true; # powers up the default Bluetooth controller on boot
-  services.blueman.enable = true;
-
-
-
-services = {
-	xserver.enable = true;
-  xserver.displayManager.gdm.enable = true;
-  xserver.desktopManager.gnome.enable = true;
-  syncthing.enable = true;
-	};
-
-environment.gnome.excludePackages = (with pkgs; [
-  gnome-photos
-  gnome-tour
-  gedit # text editor
-]);
-##############################################
-
-  # Configure keymap in X11
-  #services.xserver = {
-  #  layout = "us";
-  #  xkbVariant = "";
-  #};
-
-  # Enable CUPS to print documents.
-   #services.printing.enable = true;
+    environment.gnome.excludePackages = (with pkgs; [
+        gnome-photos
+        gnome-tour
+        gedit
+    ]);
 
   # Enable sound with pipewire.
-  hardware.pulseaudio.enable = false;
-  security.rtkit.enable = true;
-  services.pipewire = {
-    enable = true;
-    alsa.enable = true;
-    alsa.support32Bit = true;
-    pulse.enable = true;
-    # If you want to use JACK applications, uncomment this
-    #jack.enable = true;
+    hardware.pulseaudio.enable = false;
+    security.rtkit.enable = true;
+    services.pipewire = {
+        enable = true;
+        alsa.enable = true;
+        alsa.support32Bit = true;
+        pulse.enable = true;
 
-    # use the example session manager (no others are packaged yet so this is enabled by default,
-    # no need to redefine it in your config for now)
-    #media-session.enable = true;
-  };
+    };
 
-  # Enable touchpad support (enabled default in most desktopManager).
-  # services.xserver.libinput.enable = true;
-  nix.settings.experimental-features = [ "nix-command" "flakes" ];
-  # Define a user account. Don't forget to set a password with ‘passwd’.
-  users.users.amber = {
-    isNormalUser = true;
-    description = "Amber";
-    extraGroups = [ "networkmanager" "wheel" ];
-    packages = with pkgs; [
-    ];
-  };
-
-  # Enable automatic login for the user.
-  #services.xserver.displayManager.autoLogin.enable = false;
-  #services.xserver.displayManager.autoLogin = false;
-  #services.xserver.displayManager.autoLogin.user = "amber";
+    nix.settings.experimental-features = [ "nix-command" "flakes" ];
+      # Define a user account. Don't forget to set a password with ‘passwd’.
+      users.users.amber = {
+        isNormalUser = true;
+        description = "Amber";
+        extraGroups = [ "networkmanager" "wheel" ];
+        packages = with pkgs; [
+        ];
+      };
 
 
-  # Allow unfree packages
-  nixpkgs.config.allowUnfree = true;
+      nixpkgs.config.allowUnfree = true;
 
-  # List packages installed in system profile. To search, run:
-  # $ nix search wget
-  environment.systemPackages = with pkgs; [
-  unzip
-  wget
-  git
-  git-credential-manager
-  curl
-  tamsyn
-  kitty
-  wpgtk
-  flatpak
-  # applications
-  river
-  ungoogled-chromium
-  vscode
-  foot
-  ghostty
-  tmux
-  mpv
-  vlc
-  obsidian
-  discord
-  spotify
-  syncthing
-  neovim
-  gimp-with-plugins
-  anki
-  impression
-  menulibre #menu editor
-  lutris
-  syncthing
-  gh
-  android-tools #Supernote Sideloading
-  libreoffice-qt6-fresh
-  hunspell #spellcheck
-  themechanger
-  ];
+      environment.systemPackages = with pkgs; [
+      unzip
+      wget
+      git
+      git-credential-manager
+      curl
+      tamsyn
+      kitty
+      wpgtk
+      flatpak
+      # applications
+      river
+      ungoogled-chromium
+      vscode
+      foot
+      ghostty
+      tmux
+      mpv
+      vlc
+      obsidian
+      discord
+      spotify
+      syncthing
+      neovim
+      gimp-with-plugins
+      anki
+      impression
+      menulibre #menu editor
+      lutris
+      syncthing
+      gh
+      android-tools #Supernote Sideloading
+      libreoffice-qt6-fresh
+      hunspell #spellcheck
+      themechanger
+      ];
 
-  programs.steam.enable = true;
-  programs.kdeconnect.enable = true;
+      programs.steam.enable = true;
+      programs.kdeconnect.enable = true;
 
-  # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
+      # Enable the OpenSSH daemon.
+      # services.openssh.enable = true;
 
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
+      # Open ports in the firewall.
+      # networking.firewall.allowedTCPPorts = [ ... ];
+      # networking.firewall.allowedUDPPorts = [ ... ];
+      # Or disable the firewall altogether.
+      # networking.firewall.enable = false;
 
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "24.05"; # Did you read the comment?
-
+      # This value determines the NixOS release from which the default
+      # settings for stateful data, like file locations and database versions
+      # on your system were taken. It‘s perfectly fine and recommended to leave
+      # this value at the release version of the first install of this system.
+      # Before changing this value read the documentation for this option
+      # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+      system.stateVersion = "24.05"; # Did you read the comment?
 
 }
--- home.nix
+++ home.nix
@@ -2,26 +2,11 @@
 
 {
   lib.mkForce = true;
-  # Home Manager needs a bit of information about you and the paths it should
-  # manage.
   home.username = "amber";
   home.homeDirectory = lib.mkForce "/home/amber";
-  # This value determines the Home Manager release that your configuration is
-  # compatible with. This helps avoid breakage when a new Home Manager release
-  # introduces backwards incompatible changes.
-  #
-  # You should not change this value, even if you update Home Manager. If you do
-  # want to update the value, then make sure to first check the Home Manager
-  # release notes.
   home.stateVersion = "23.11"; # Please read the comment before changing.
 
-  
-
-  # The home.packages option allows you to install Nix packages into your
-  # environment.
   home.packages = [
-    # # Adds the 'hello' command to your environment. It prints a friendly
-    # # "Hello, world!" when run.
     pkgs.hyprland
     pkgs.wofi
     pkgs.yt-dlp
@@ -29,37 +14,29 @@
     pkgs.pywal
     pkgs.wl-clipboard
     pkgs.protonvpn-gui
-    # # It is sometimes useful to fine-tune packages, for example, by applying
-    # # overrides. You can do that directly here, just don't forget the
-    # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
-    # # fonts?
-    # (pkgs.nerdfonts.override { fonts = [ "FantasqueSansMono" ]; })
 
-    # # You can also create simple shell scripts directly inside your
-    # # configuration. For example, this adds a command 'my-hello' to your
-    # # environment:
-     (pkgs.writeShellScriptBin "vgrab" ''
+    (pkgs.writeShellScriptBin "vgrab" ''
       yt-dlp -o "%(title)s.%(ext)s" -f "bv*[height<=1080]+ba/b[height<=1920]" -P "~/Downloads" $1
      '')
   ];
 
 
-gtk = {
- enable = true;
-    theme = {
-      name = "rose-pine";
-      package = pkgs.rose-pine-gtk-theme;
+    gtk = {
+     enable = true;
+        theme = {
+          name = "rose-pine";
+          package = pkgs.rose-pine-gtk-theme;
+        };
     };
-};
 
-qt = {
-  enable = true;
-  platformTheme.name = "gtk2";
-  style = {
-    name = "rose-pine";
-    package = pkgs.rose-pine-gtk-theme;
- };
-};
+    qt = {
+        enable = true;
+        platformTheme.name = "gtk2";
+        style = {
+        name = "rose-pine";
+        package = pkgs.rose-pine-gtk-theme;
+        };
+    };
 
   dconf = {
     enable = true;
@@ -70,20 +47,7 @@ qt = {
     };
   };
 
-  # Home Manager is pretty good at managing dotfiles. The primary way to manage
-  # plain files is through 'home.file'.
   home.file = {
-    # # Building this configuration will create a copy of 'dotfiles/screenrc' in
-    # # the Nix store. Activating the configuration will then make '~/.screenrc' a
-    # # symlink to the Nix store copy.
-    # ".screenrc".source = dotfiles/screenrc;
-
-    # # You can also set the file content immediately.
-    # ".gradle/gradle.properties".text = ''
-    #   org.gradle.console=verbose
-    #   org.gradle.daemon.idletimeout=3600000
-    # '';
-    
     ".config/foot/foot.ini".source = ./foot/foot.ini;
     ".config/hypr/hyprland.conf".source = ./hypr/hyprland.conf;
     ".config/nvim/init.lua".source = ./nvim/init.lua;
@@ -93,6 +57,7 @@ qt = {
     ".config/nvim/lua/config/lazy.lua".source = ./nvim/lua/config/lazy.lua;
     ".config/nvim/lua/plugins/vim-tmux-navigator.lua".source = ./nvim/lua/plugins/vim-tmux-navigator.lua;
     ".config/nvim/lua/plugins/telescope.lua".source = ./nvim/lua/plugins/telescope.lua;
+    ".config/nvim/lua/plugins/lsp.lua".source = ./nvim/lua/plugins/lsp.lua;
   };
 
   # Home Manager can also manage your environment variables through
@@ -117,7 +82,6 @@ qt = {
 
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
-  #programs.home-manager.path = "$HOME/dotfiles";
 
   programs.git = {
     enable = true;
